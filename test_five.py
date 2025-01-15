8e3af15eb7627c75ec2798a551644164c59590f8
Suba Narayanan <subanarayanan@Subas-Air.hsd1.ca.comcast.net>
2025-01-14 21:59:12 -0800

    Chapter 1

---
+++ test_five.py
@@ -0,0 +1,4 @@
+def test_stringassert():
+    a="hello"
+    b="hello world"
+    assert a not in b
---
+++ test_two.py
@@ -0,0 +1,4 @@
+def test_failing():
+    assert (1, 2, 3) == (3, 2, 1)
+    # to run this test, run the following command in the terminal
+    # pytest test_two.py
